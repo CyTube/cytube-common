8692e9327f335ecc3fe8f218678163473057ebd0
calzoneman <calzoneman@gmail.com>
2016-01-26 22:29:55 -0800

    Add EVALSHA support to lualoader

--- src/redis/lualoader.js
+++ src/redis/lualoader.js
@@ -1,12 +1,29 @@
 import fs from 'fs';
+import logger from '../logger';
 
 const CACHE = {};
+const EVALSHA_CACHE = {};
 
-export default function loadLuaScript(filename) {
+export function loadLuaScript(filename) {
     if (CACHE.hasOwnProperty(filename)) {
         return CACHE[filename];
     }
 
     CACHE[filename] = fs.readFileSync(filename).toString('utf8');
     return CACHE[filename];
+}
+
+export function runLuaScript(redisClient, filename, args) {
+    if (EVALSHA_CACHE.hasOwnProperty(filename)) {
+        args.unshift(EVALSHA_CACHE[filename]);
+        return redisClient.evalshaAsync.apply(redisClient, args);
+    } else {
+        return redisClient.scriptAsync('load', loadLuaScript(filename))
+                .then(sha => {
+            logger.debug(`Cached ${filename} as ${sha}`);
+            EVALSHA_CACHE[filename] = sha;
+            args.unshift(sha);
+            return redisClient.evalshaAsync.apply(redisClient, args);
+        });
+    }
 }
